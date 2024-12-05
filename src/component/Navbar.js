4f424b08db4f7f547a2aab6b6022928d9f0d1ac0
Lee <cookie3013@naver.com>
2024-12-05 10:35:50 +0900

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,31 @@
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+
+
+    const activateStyle = {
+        backgroundColor: "black",
+        color:"white"
+    };
+
+
+    return(
+        <>
+        <ul>
+            <li>
+                <NavLink to="/" style={({isActive})=>
+                isActive? activateStyle : null
+            }>Home</NavLink>
+            </li>
+            <li>
+                <NavLink to="/menupage" style={({isActive})=>
+                isActive? activateStyle : null
+            }>MenuPage</NavLink>
+            </li>
+        </ul>
+        </>
+    )
+
+}
+
+export default Navbar;
---
+++ src/pages/MenuPage.js
@@ -0,0 +1,9 @@
+const MenuPage = () => {
+    return(
+        <>
+        <h1>우리가 메뉴를 불러와서 봉려줄 페이지</h1>
+        </>
+    );
+};
+
+export default MenuPage;
