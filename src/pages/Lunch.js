d2d56a57741b65142dc1b730367918f30d57ab41
Lee <cookie3013@naver.com>
2024-12-20 10:39:26 +0900

    dd

--- src/pages/Lunch.js
+++ src/pages/Lunch.js
@@ -13,7 +13,11 @@ const LoadLunch = async (e) => {
   try {
     const response = await fetch(
       'https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=e2d93272fec44184b467b6accf92f079&ATPT_OFCDC_SC_CODE=T10&SD_SCHUL_CODE=9290083',
-      { method: 'GET' }
+      { method: 'GET',
+        headers: {
+          'Accept': 'application/json',  // 응답으로 JSON을 요구
+        }
+       }
     );
 
     if (!response.ok) {
